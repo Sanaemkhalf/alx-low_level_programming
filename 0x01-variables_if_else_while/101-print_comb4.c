cb99cf1ea49a59182640d2f6e723dfa4a7554d6a
sanae mkhalfi <sanaemkhalfi@gmail.com>
2023-12-21 06:03:53 -0800

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,16 +14,16 @@ int main(void)
 
 	for (d = '0'; d < '9'; d++)
 	{
-	
+
 	for (p = d + 1; p <= '9'; p++)
 	{
-	
+
 	for (q = p + 1; q <= '9'; q++)
 	{
-	
+
 	if ((p != d) != q)
 	{
-	
+
 	putchar(d);
 	putchar(p);
 	putchar(q);
