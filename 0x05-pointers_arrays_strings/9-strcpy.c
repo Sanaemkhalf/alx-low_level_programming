55d0671902abc98a37d55b15be0bbd6d8f88e427
sanae mkhalfi <sanaemkhalfi@gmail.com>
2024-01-03 10:59:28 -0800

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strcpy - Copy a string
+ * _strcpy - Copy a string
  * @dest: Destination value
  * @src: Source value
  * Return: the pointer to dest char
@@ -11,6 +11,7 @@ char *_strcpy(char *dest, char *src)
 
 {
 	int i;
+
 	for (i = 0; src[i] != '\0'; i++)
 	{
 	dest[i] = src[i];
