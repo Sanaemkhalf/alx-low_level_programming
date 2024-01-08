7309c08a375d447ec0d0e346395bbffe67a15ec2
sanae mkhalfi <sanaemkhalfi@gmail.com>
2024-01-08 02:53:54 -0800

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *_strcat - Concatemates two strings
+ * _strcat - Concatemates two strings
  * @dest: The destination string
  * @src: The source string
  * Rerurn: A pointer to the resulting string dest
@@ -9,22 +9,23 @@
 
 char *_strcat(char *dest, char *src)
 {
-int k, r;
 
-k = 0;
-r = 0;
+	int k, r;
 
-while (dest[k] != '\0')
-{
+	k = 0;
+	r = 0;
+
+	while (dest[k] != '\0')
+	{
 	k++;
-}
+	}
 
-while (src[r] != '\0')
-{
-	dest[k] = src[r];
-	k++;
-	r++;
-}
-dest[k] = '\0';
-return (dest);
+	while (src[r] != '\0')
+	{
+		dest[k] = src[r];
+		k++;
+		r++;
+	}
+	dest[k] = '\0';
+	return (dest);
 }
