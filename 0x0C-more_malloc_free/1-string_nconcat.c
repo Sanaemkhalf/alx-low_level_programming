f1476414f40136095cc8daafad6d773ca0591ebd
sanae mkhalfi <sanaemkhalfi@gmail.com>
2024-01-18 12:06:08 -0800

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * _string_nconcat - duplicate to new memory space location
+ * string_nconcat - duplicate to new memory space location
  * @str: char
  * Return: 0
  */
@@ -10,19 +10,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i = 0, j = 0, k = 0, l = 0;
 	char *str;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
 	while (s1[i])
 		i++;
+
 	while (s2[k])
 		k++;
+
 	if (n >= k)
 		l = i + k;
 	else
 		l = i + n;
-	str = malloc(sizeof(char) * l +1);
+
+	str = malloc(sizeof(char) * l + 1);
+
 	if (str == NULL)
 		return (NULL);
 	k = 0;
@@ -30,10 +36,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		if (j <= i)
 			str[j] = s1[j];
+
 		if (j >= i)
 		{
 			str[j] = s2[k];
-		k++;
+			k++;
 		}
 		j++;
 	}
